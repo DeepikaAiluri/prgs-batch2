3a21ecb9bc36ad9a62f229be1e47c2e7690d6b2b
DeepikaAiluri <deepika4sri@mail.com>
2019-05-03 13:33:19 -0500

    Changes to be committed:

--- Cal.java
+++ Cal.java
@@ -5,7 +5,38 @@ public class Cal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("its an assignment program");
-	}
+		System.out.println("Its an assignment program");
+		
+		
+	    	 
+	    	 int a=5,b=5;
+	    	 System.out.println("For multiplication");
+	    	 System.out.println("the  result is " + a*b);
+	    	 
+	   
 
+		getsum();
+		getdifference();
+		division();
+		
+			
+		}
+	
+	public static void getsum(){
+		int a=100,b=20;
+		int c=a+b;
+		System.out.println("The total is"+ c);
+	}
+     public static void getdifference(){
+	int a=100,b=20;
+	int c=a-b;
+	System.out.println("the difference is "+ c);
+	
 }
+     public static void division(){
+    	 int a=10,b=2;
+    	 System.out.println("the result is"+ a/b);
+    	 
+    	 
+     }
+    }
