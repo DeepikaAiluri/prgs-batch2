b2eaf53c62601c3749ec286f0342a87d03f88a06
DeepikaAiluri <deepika4sri@mail.com>
2019-05-03 12:58:17 -0500

    Initial commit

---
+++ Cal.java
@@ -0,0 +1,11 @@
+package caluculate;
+
+public class Cal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("its an assignment program");
+	}
+
+}
